e1f46e1b3f6563d282d3c4792109e3ab796b0add
anushasaikam <asaikam25@gmail.com>
2025-05-02 15:32:34 +0100

    modified LoginPage

--- src/main/java/com/automation/pages/LoginPage.java
+++ src/main/java/com/automation/pages/LoginPage.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
-   private WebDriver driver;
+   private final WebDriver driver;
 
     // Locators
-    private By usernameField = By.id("user-name");
-    private By passwordField = By.id("password");
-    private By loginButton = By.id("login-button");
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login-button");
 
     // Constructor
     public LoginPage(WebDriver driver) {
